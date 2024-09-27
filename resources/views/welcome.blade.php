ab37120f4d1068caa12e72490f50e7142f20a31c
OdalisAbreu <odalisdabreu@gmail.com>
2024-09-27 17:27:53 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thessalus V1</title>
+    @viteReactRefresh
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div id="root"></div>
+</body>
+
+</html>
